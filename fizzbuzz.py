dec0a3b33b45dad5dc4162ce260d0ae3dea40708
Evan Warden <erwgny@umsystem.edu>
2023-03-06 21:55:24 -0600

    first commit

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+num = 1
+
+while (num <= 100):
+    if (num % 3 == 0) and (num % 5 == 0):
+        print(str(num) + ". Fizzbuzz")
+    elif (num % 3 == 0):
+        print(str(num) + ". Fizz")
+    elif (num % 5 == 0):
+        print(str(num) + ". Buzz")
+    else:
+        print(str(num) + '.')
+
+    num += 1
